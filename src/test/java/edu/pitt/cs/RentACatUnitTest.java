570d5ad0eb9087cc832b2bd3966ed22754816242
Tassneem Khattab <tsk37@pitt.edu>
2024-09-18 12:26:54 -0400

    mock

--- src/test/java/edu/pitt/cs/RentACatUnitTest.java
+++ src/test/java/edu/pitt/cs/RentACatUnitTest.java
@@ -46,7 +46,7 @@ public class RentACatUnitTest {
 
 
 		// Create a mock Cat instance
-		r = RentACat.createInstance(InstanceType.IMPL);
+		r = RentACat.createInstance(InstanceType.MOCK);
 
 		c1 = Mockito.mock(Cat.class);
 		when(c1.getId()).thenReturn(1);
